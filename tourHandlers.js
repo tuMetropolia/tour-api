9747fcde885b9c8591b7d8a7033e88e60fcfa059
Tu Dinh <stella.dinh1206@gmail.com>
2024-09-02 15:16:13 +0300

    Activity 2 with Alex

--- tourHandlers.js
+++ tourHandlers.js
@@ -1,23 +1,48 @@
 const Tour = require("./tourLib");
 
 const getAllTours = (req, res) => {
-  res.json({ message: "Hello from getAllTours" });
+  const tours = Tour.getAll();
+  res.json(tours);
 };
 
 const createTour = (req, res) => {
-  res.json({ message: "Hello from createTour" });
+  const {name, info, image, price} = req.body;
+  const newTour = Tour.addOne(name, info, image, price);
+  if (newTour) {
+    res.json(newTour);
+  } else {
+    json.status(500).json({message: "Invalid input"});
+  }
 };
 
 const getTourById = (req, res) => {
-  res.json({ message: "Hello from getTourById" });
+  const tourFound = Tour.findById(req.params.tourId);
+  if (tourFound) {
+    res.json(tourFound);
+  } else {
+    json.staus(404).json({message: "Tour not found"});
+  }
 };
 
 const updateTour = (req, res) => {
-  res.json({ message: "Hello from updateTour" });
+  const tourId = req.params.tourId;
+  const {name, info, image, price} = req.body;
+  const updateTour = Tour.updateOneById(tourId, {name, info, image, price});
+  if (updateTour) {
+    res.json(updateTour);
+  } else {
+    res.status(404).json({message: "Invalid input"})
+  }
 };
 
 const deleteTour = (req, res) => {
-  res.json({ message: "Hello from deleteTour" });
+  const tourId = req.params.tourId;
+  const deletedTour = Tour.deleteOneById(tourId);
+  if (deletedTour) {
+    res.json({message: "Tour deleted"});
+  } else {
+    res.status(404).json({mesage: "Tour not found"});
+  }
 };
 
 module.exports = {
--- tourLib.js
+++ tourLib.js
@@ -1,3 +1,90 @@
-if (require.main === module) {
-  console.log("getAll called:", getAll());
+// {
+//   "name": "Best of Paris in 7 Days Tour",
+//   "info": "Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!",
+//   "image": "https://www.course-api.com/images/tours/tour-1.jpeg",
+//   "price": "1,995"
+// }
+
+let tours= [];
+let nextId = 1;
+
+const getAll = () => {
+  return tours;
 }
+const addOne = (name, info, image, price) => {
+  if (!name || !info|| !image || !price ) {
+    return false;
+  }
+  const tour = {id: nextId++, name, info, image, price};
+  tours.push(tour);
+  return tour;
+}
+const findById = (id) => {
+  const numbericId = Number(id);
+  const tour = tours.find(tour => tour.id === numbericId)
+  return tour ? tour : false;
+}
+
+const updateOneById = (id, tourData) => {
+  const tourFound = findById(id);
+  if (tourFound) {
+    if (tourData.name) {
+      tourFound.name = tourData.name;
+    }    
+    if (tourData.info) {
+      tourFound.info = tourData.info;
+    }
+    if (tourData.image) {
+      tourFound.image = tourData.image;
+    }
+    if (tourData.price) {
+      tourFound.price = tourData.price;
+    }
+    return tourFound;
+  } return false;
+}
+
+const deleteOneById = (id) => {
+  const deletedTour = findById(id);
+  if (deletedTour) {
+    const initLength = tours.length;
+    tours = tours.filter(tour => tour.id !== deletedTour.id); 
+    return tours.length < initLength;
+  }
+  return false;
+}
+
+if (require.main === module) {
+  // Add tour
+  let result = addOne(
+    "Best of Paris in 7 Days Tour",
+    "Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!",
+    "https://www.course-api.com/images/tours/tour-1.jpeg",
+    "1,995"
+  ); ;
+  console.log(result);
+  // Add another car
+  result = addOne("tour name 2", "info2", "image2", "price");
+  console.log(result);
+
+  console.log("getAll called:", getAll());
+
+  console.log("findById called:", findById(1));
+
+  console.log(
+    "updateOneById called:",
+    updateOneById(1, {name: "name123"})
+  );
+  console.log("findById called after item updated:", findById(1));
+
+  console.log("deleteOneById called:", deleteOneById(1));
+  console.log("findById called after item deleted:", findById(1));
+}
+
+module.exports = {
+  getAll,
+  addOne,
+  findById,
+  updateOneById,
+  deleteOneById,
+};
